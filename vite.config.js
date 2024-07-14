7e5ea0313b634442dd92883af457adc7b289b619
Elijahcletuz <cletuselijah01@gmail.com>
2024-07-14 18:20:59 +0100

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build'
+  },
   server: {
     proxy: {
-      "/api": {
-        target: "https://api.timbu.cloud",
+      '/api': {
+        target: 'https://api.timbu.cloud',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
